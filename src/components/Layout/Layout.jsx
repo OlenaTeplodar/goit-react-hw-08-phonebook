bb40fc2a2e5b18cff414779ce5377acec0062f88
olenaTeplodar <lena.golubenkova@gmail.com>
2023-03-18 00:28:02 +0200

    upgrade

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,8 +2,9 @@ import { Outlet } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { Header } from 'components/Header';
 import { Suspense } from 'react';
-import { Container, Typography, Link } from '@mui/material';
+import { Container  } from '@mui/material';
 import { FadeLoader } from 'react-spinners';
+// import { Typography, Link } from '@mui/material';
 
 export const Layout = () => {
   return (
